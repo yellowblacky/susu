6e144dd04f028900c7c0fe92c5732878f9d51330
yellowblacky <147934051+yellowblacky@users.noreply.github.com>
2024-08-23 14:33:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const ownerNumber = ['94757501723']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
-if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
+if(!config.SESSION_ID) return console.log('PRABATH-MD~QndFxSZb#lpGtwzZlO9C5rVzBwvKLIOXPAt7B8fDzcB5RIygqmQ0')
 const sessdata = config.SESSION_ID
 const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
 filer.download((err, data) => {
